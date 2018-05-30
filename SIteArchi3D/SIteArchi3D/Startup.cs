a60b013f0fa0dd2386a37ad7d8f219e1f1cfe478
Sherazade B <sherazade.b86@gmail.com>
2018-05-31 00:53:38 +0200

    Added new version of project + new better databases

---
+++ SIteArchi3D/SIteArchi3D/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SIteArchi3D.Startup))]
+namespace SIteArchi3D
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
